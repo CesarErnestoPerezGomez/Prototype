5d58de8734c22c273b9cbcf953b21ea0995c9578
Jorge12 <0250990@up.edu.mx>
2024-11-28 18:11:24 -0600

    profile correcto

--- house-catalog/src/components/profile.jsx
+++ house-catalog/src/components/profile.jsx
@@ -16,13 +16,24 @@ const Profile = () => {
     fetchProfile();
   }, []);
 
+  const logout = async () => {
+    try {
+      const response = await axios.post('http://localhost:3001/logout', {}, { withCredentials: true });
+      console.log(response.data);
+      // Redirigir o hacer algo después del cierre de sesión
+    } catch (error) {
+      console.log("Error al cerrar sesión", error);
+    }
+  };
+
   if (!user) return <div>Loading...</div>;
 
   return (
     <div>
       <h2>Welcome, {user.name}</h2>
       <p>Email: {user.email}</p>
-      
+      // En tu componente, usa esta función
+<button onClick={logout}>Cerrar sesión</button>
     </div>
     
   );
